008ea5ddebff15fa89d9b8fd93caf60b4f8de7e6
Administrator <Administrator@PC-20200126VCZA>
2020-03-05 10:36:59 +0800

    springboot_activi

---
+++ src/main/java/com/yawn/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.yawn.controller;
+
+import com.yawn.entity.User;
+import com.yawn.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Created by yawn on 2018-01-08 13:39
+ */
+@Controller
+public class LoginController {
+
+    @Resource
+    private UserService userService;
+
+    @PostMapping("/login")
+    @ResponseBody
+    public boolean login(HttpSession session, @RequestBody User user) {
+        String userName = user.getUserName();
+        String password = user.getPassword();
+        boolean login = userService.login(userName, password);
+      //  login=true;
+        if (login) {
+            session.setAttribute("userName", userName);
+            return true;
+        }
+        session.setAttribute("userName", userName);
+        return true;
+    }
+
+    @GetMapping("/exit")
+    public String exit(HttpSession session) {
+        session.removeAttribute("userName");
+        return "login";
+    }
+}
